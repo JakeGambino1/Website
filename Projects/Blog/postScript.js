3159a6a0ddef08c72041850c54c2c5bd29d68eeb
Jake Gambino <37968364+JakeGambino1@users.noreply.github.com>
2018-04-26 08:51:39 -0500

    Updates, added genericons, initial social media slider

--- Projects/Blog/postScript.js
+++ Projects/Blog/postScript.js
@@ -1,16 +1,12 @@
 const focus = document.querySelector('#contentFocus');
-const htmlFocus = document.querySelector('#html');
-const cssFocus = document.querySelector('#css');
-const bootstrapFocus = document.querySelector('#bootstrap');
-const javascriptFocus = document.querySelector('#javascript');
 
-function contentFocus() {
-  focus.innerHTML = htmlFocus.innerHTML;
-  console.log('trigger');
+function focusContent(newFoc) {
+  console.log('Trigger before while loop');
+  focus.innerHTML = '';
+  console.log('trigger after innerHTML clear');
+  while (newFoc.childNodes.length > 0) {
+    console.log('trigger after initial while');
+    focus.appendChild(newFoc.childNodes[0]);
+    console.log('Done');
+  }
 }
-
-// function contentFocus() {
-//   let newFocus = document.querySelector(this)
-//   focus.innerHTML = document.querySelector('#html');
-//   console.log('trigger');
-// }
